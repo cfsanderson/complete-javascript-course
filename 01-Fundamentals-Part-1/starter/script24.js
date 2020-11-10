333c5136a5aa1d413ccbbe00b08068869e070fe6
caleb-sni <caleb.sanderson@spatialnetworks.com>
2020-11-09 21:38:48 -0500

    up to video 24

--- 01-Fundamentals-Part-1/starter/script24.js
+++ 01-Fundamentals-Part-1/starter/script24.js
@@ -1,5 +1,23 @@
 const hasDriverLicense = true; // A
-const hasGoodvision = false; // B
+const hasGoodvision = true; // B
 
 console.log(hasDriverLicense && hasGoodvision);
 console.log(hasDriverLicense || hasGoodvision);
+console.log(!hasDriverLicense);
+
+const shouldDrive = hasDriverLicense && hasGoodvision;
+
+// if (hasDriverLicense && hasGoodvision) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive..");
+// }
+
+const isTired = false; // C
+console.log(hasDriverLicense || hasGoodvision || isTired);
+
+if (hasDriverLicense && hasGoodvision && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive..");
+}
